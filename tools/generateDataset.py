6cb664467bbc03a28f8e64fb08ef7db277abd694
Bernardo Taveira <taveira@student.chalmers.se>
2024-02-24 12:49:52 +0100

    generateDataset: add options for transforming and using global frae pointcloud

--- tools/generateDataset.py
+++ tools/generateDataset.py
@@ -26,12 +26,21 @@ CAR_STATE_TOPIC = "/ekf/car_state"
 POINTCLOUD_TOPIC = "/ouster/points"
 
 class DatasetGenerator:
-    def __init__(self, selected_file: str, lidar_rotation: float = 1.5707, label_every: int = 10):
+    def __init__(self,
+                 selected_file: str,
+                 lidar_rotation: float = 1.5707,
+                 label_every: int = 10,
+                 pc_in_global_frame: bool = False,
+                 ego_motion_compensate: bool = False
+                 ):
         self.selected_file: str = selected_file
         self.lidar_rotation: float = lidar_rotation
         self.label_every: int = label_every
+        self.pc_in_global_frame: bool = pc_in_global_frame
+        self.ego_motion_compensate: bool = ego_motion_compensate
 
-    def transform_point_cloud(self, point_cloud: np.ndarray, vx: float, vy: float, yaw_rate: float) -> np.ndarray:
+    def _compensate_point_cloud(self, point_cloud: np.ndarray, vx: float, vy: float, yaw_rate: float) -> np.ndarray:
+        """Perform ego-motion compensation on the point cloud."""
         lidarFrequency = 20  # Hz
         nPositions = 100  # Number of positions to compute over
         timeStep = 1.0 / (lidarFrequency * nPositions)  # The time step between positions
@@ -73,7 +82,31 @@ class DatasetGenerator:
 
         return pcl_cloud_transformed
 
-    def get_local_cones(self, cones, pose,  pc_array: np.ndarray) -> Tuple[List[Tuple[float, float]], float]:
+    def _get_local_pcl(self, 
+                       pcl: np.ndarray,
+                       pose: Any
+                       ) -> np.ndarray:
+        # Extract pose components
+        x, y = pose.position.x, pose.position.y
+
+        orientation_list = [
+            pose.orientation.x,
+            pose.orientation.y,
+            pose.orientation.z,
+            pose.orientation.w,
+        ]
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+
+        # Tranform pointcloud from global to local frame
+        if self.pc_in_global_frame:
+            for i in range(pcl.shape[0]):
+                x_pc = pcl[i, 0]
+                pcl[i, 0] = (x_pc - x) * math.cos(yaw) + (pcl[i, 1] - y) * math.sin(yaw)
+                pcl[i, 1] = -(x_pc - x) * math.sin(yaw) + (pcl[i, 1] - y) * math.cos(yaw)
+
+        return pcl
+
+    def _get_local_cones(self, cones, pose,  pc_array: np.ndarray) -> Tuple[List[Tuple[float, float]], float]:
         # Extract pose components
         x, y = pose.position.x, pose.position.y
 
@@ -94,13 +127,13 @@ class DatasetGenerator:
             x_rel = (global_x - x) * math.cos(yaw) + (global_y - y) * math.sin(yaw)
             y_rel = -(global_x - x) * math.sin(yaw) + (global_y - y) * math.cos(yaw)
 
-            # Check if it contains more than 4 points in the point cloud within a radius of 0.2 units
-            if np.sum((pc_array[:, 0] - x_rel) ** 2 + (pc_array[:, 1] - y_rel) ** 2 < 0.5 ** 2) >= 1:
-                local_cones.append((x_rel, y_rel))
+            local_cones.append((x_rel, y_rel))
 
         return local_cones, yaw
 
-    def read_data(self, global_cones: List[Tuple[float, float]]) -> Generator[Tuple[np.ndarray, List[Tuple[float, float]], Dict[str, Any]], None, None]:
+    def _read_data(self,
+                   global_cones: List[Tuple[float, float]]
+                   ) -> Generator[Tuple[np.ndarray, List[Tuple[float, float]], Dict[str, Any]], None, None]:
         reader = rosbag2_py.SequentialReader()
         reader.open(
             rosbag2_py.StorageOptions(uri=self.selected_file, storage_id="mcap"),
@@ -155,21 +188,31 @@ class DatasetGenerator:
 
                 msg_type = get_message(typename(topic))
                 msg = deserialize_message(data, msg_type)
-                pc_data = pc2.read_points(msg, field_names=("x", "y", "z", "intensity", "t"), skip_nans=True)
-                pc_array = np.array([list(p) for p in pc_data[['x', 'y', 'z', 'intensity', 't']]], dtype=np.float32).reshape(-1, 5)
+
+                if self.ego_motion_compensate:
+                    pc_data = pc2.read_points(msg, field_names=("x", "y", "z", "intensity", "t"), skip_nans=True)
+                    pc_array = np.array([list(p) for p in pc_data[['x', 'y', 'z', 'intensity', 't']]], dtype=np.float32).reshape(-1, 5)
+                else:
+                    pc_data = pc2.read_points(msg, field_names=("x", "y", "z", "intensity"), skip_nans=True)
+                    pc_array = np.array([list(p) for p in pc_data[['x', 'y', 'z', 'intensity']]], dtype=np.float32).reshape(-1, 4)
 
                 for i in range(pc_array.shape[0]):
-                    x = - pc_array[i, 0] * math.cos(self.lidar_rotation) + pc_array[i, 1] * math.sin(self.lidar_rotation) + 1.0
-                    pc_array[i, 1] = - pc_array[i, 0] * math.sin(self.lidar_rotation) - pc_array[i, 1] * math.cos(self.lidar_rotation)
+                    x = pc_array[i, 0] * math.cos(self.lidar_rotation) - pc_array[i, 1] * math.sin(self.lidar_rotation) + 1.0
+                    pc_array[i, 1] = pc_array[i, 0] * math.sin(self.lidar_rotation) + pc_array[i, 1] * math.cos(self.lidar_rotation)
                     pc_array[i, 0] = x
                     pc_array[i, 2] = pc_array[i, 2] + 0.45
                 pc_array = pc_array.astype(np.float32)
 
+                # Transform pointcloud from global to local frame
+                if self.pc_in_global_frame:
+                    pc_array = self._get_local_pcl(pc_array, pose)
+
                 # Point-wise ego motion compensation (x, y and yaw rate)
-                pc_array = self.transform_point_cloud(pc_array, vx, vy, yawrate)
+                if self.ego_motion_compensate:
+                    pc_array = self._compensate_point_cloud(pc_array, vx, vy, yawrate)
                 pc_array = pc_array[:, :4]
 
-                cones, yaw = self.get_local_cones(global_cones, pose, pc_array)
+                cones, yaw = self._get_local_cones(global_cones, pose, pc_array)
                 odom = {
                     "timestamp": msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9,
                     "x": pose.position.x,
@@ -216,7 +259,7 @@ class DatasetGenerator:
         }
 
         unlabeled_clouds = []
-        for i, (pcd, cones, odom) in enumerate(self.read_data(global_cones)):
+        for i, (pcd, cones, odom) in enumerate(self._read_data(global_cones)):
             # Ignore first one since it has not previous lidar scans
             if i == 0:
                 continue
@@ -231,7 +274,10 @@ class DatasetGenerator:
                     f.write(pcd.tobytes())
 
                 # Get the cones in the local frame
-                x_values, y_values = zip(*cones)
+                if not cones:
+                    x_values, y_values = [], []
+                else:
+                    x_values, y_values = zip(*cones)
                 # Save to file
                 with open(f"{folder_path}/labels/{filenum:07d}.txt", "w") as f:
                     for x, y in zip(x_values, y_values):
@@ -290,12 +336,19 @@ class DatasetGenerator:
 
 
 class MapEditor:
-    def __init__(self, lidar_rotation: float = 1.5707, label_every: int = 10):
+    def __init__(self,
+                 lidar_rotation: float = 1.5707,
+                 label_every: int = 10,
+                 pc_in_global_frame: bool = False,
+                 ego_motion_compensate: bool = False
+                 ):
         self.cones: List[Tuple[float, float]] = []
         self.selected_cones: Set[Tuple[float, float]] = set()
         self.selected_file: str = ""
         self.lidar_rotation: float = lidar_rotation
         self.label_every: int = label_every
+        self.pc_in_global_frame: bool = pc_in_global_frame
+        self.ego_motion_compensate: bool = ego_motion_compensate
 
         self.root = tk.Tk()
         self.root.title("Map editor")
@@ -457,7 +510,9 @@ class MapEditor:
             messagebox.showinfo("Info", "Select cones to remove.")
 
     def gen_data(self):
-        datasetGenerator = DatasetGenerator(self.selected_file, self.lidar_rotation, self.label_every)
+        datasetGenerator = DatasetGenerator(self.selected_file, self.lidar_rotation,
+                                            self.label_every, self.pc_in_global_frame, 
+                                            self.ego_motion_compensate)
         datasetGenerator.gen_data(self.cones)
 
     def run(self):
@@ -466,15 +521,18 @@ class MapEditor:
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Map editor")
-    parser.add_argument("--lidar_rotation", type=float, default=1.5707, help="Lidar rotation")
-    parser.add_argument("--label_every", type=int, default=10, help="Label every nth point cloud")
+    parser.add_argument("--lidar-rotation", type=float, default=-1.5707, help="Lidar rotation")
+    parser.add_argument("--label-every", type=int, default=10, help="Label every nth point cloud")
+    parser.add_argument("--pc-in-global-frame", action="store_true", help="Indicates that the pointcloud is in global frame. This will transform it to local frame.")
+    parser.add_argument("--ego-motion-compensate", action="store_true", help="Ego-motion compensate the point cloud")
+
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
-    editor = MapEditor(args.lidar_rotation, args.label_every)
+    editor = MapEditor(args.lidar_rotation, args.label_every, args.pc_in_global_frame, args.ego_motion_compensate)
     editor.run()
 
 if __name__ == "__main__":
